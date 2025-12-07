b9d7a6838af8ace8a89679862629630f16c02d6d
Juliano Pereira <juliano340@gmail.com>
2025-12-07 10:35:12 -0300

    feat: integra módulo de usuários ao módulo de autenticação e configura JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  // Método de login
+  async login(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    };
+
+    const token = await this.jwtService.signAsync(payload);
+
+    return { access_token: token };
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,9 +8,6 @@ import { User } from './entities/user.entity';
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
-})
-@Module({
-  controllers: [UsersController],
-  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
